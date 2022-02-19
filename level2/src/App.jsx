3098d0f54676953ccfca76b10842bb3a962572dd
Nguyen Son Arsenal <vanson297.nguyen@gmail.com>
2022-02-19 12:17:39 +0700

    l2: add sort name and datetime UTC

--- level2/src/App.jsx
+++ level2/src/App.jsx
@@ -9,6 +9,32 @@ const mapStateToProps = store => ({
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            timeZone: 'Asia/Ho_Chi_Minh',
+            skills: this.props.skills
+        }
+    }
+
+    changeTimeZone(e) {
+        this.setState({
+            timeZone: e.target.getAttribute('id')
+        });
+    }
+
+    sort(e) {
+        let newSkills = [];
+        let sortType = e.target.getAttribute('data-sort-type');
+        let column = e.target.getAttribute('data-column');
+
+        if (sortType == 'asc') {
+            newSkills = [].concat(this.state.skills).sort((a, b) => a[column] >= b[column] ? 1 : -1);
+        } else {
+            newSkills = [].concat(this.state.skills).sort((a, b) => a[column] < b[column] ? 1 : -1);
+        }
+
+        this.setState({
+            skills: newSkills
+        })
     }
 
     render() {
@@ -16,16 +42,20 @@ class App extends React.Component {
             <div className="wrapper">
                 <div className="choose-timezone">
                     <span>Timezone: </span>
-                    <input type="radio" name="timezone" value="" id="Asia/Tokyo"/>
-                    <label htmlFor="Asia/Tokyo">Tokio</label>&nbsp;&nbsp;
-                    <input type="radio" name="timezone" value="" id="Asia/HaNoi" defaultChecked/>
-                    <label htmlFor="Asia/HaNoi">Ha Noi</label>&nbsp;&nbsp;
-                    <input type="radio" name="timezone" value="" id="Asia/London"/>
-                    <label htmlFor="Asia/London">London</label>
+                    <input type="radio" name="timezone" value="" id="Asia/Tokyo" onChange={this.changeTimeZone.bind(this)}/>
+                    <label htmlFor="Asia/Tokyo">Tokyo</label>&nbsp;&nbsp;
+                    <input type="radio" name="timezone" value="" id="Asia/Ho_Chi_Minh" defaultChecked onChange={this.changeTimeZone.bind(this)}/>
+                    <label htmlFor="Asia/Ho_Chi_Minh">Ha Noi</label>&nbsp;&nbsp;
+                    <input type="radio" name="timezone" value="" id="Europe/London" onChange={this.changeTimeZone.bind(this)}/>
+                    <label htmlFor="Europe/London">London</label>
+                </div>
+                <br/>
+                <div style={{color: 'red'}}>
+                    <small>Now: { moment().tz(this.state.timeZone).format('DD-MM-YYYY LTS') }</small>
                 </div>
                 <div className="list-control">
                     {
-                        this.props.skills.map(item => (
+                        this.state.skills.map(item => (
                             <div className="control" key={item.id}>
                                 <div className="thumb-avatar">
                                     <img src={item.avatar} alt="image name"/>
@@ -36,15 +66,19 @@ class App extends React.Component {
                                         <span className="max-character-length">{item.tagline}&nbsp;&nbsp;</span>
                                         <a className="edit" href="#">Edit</a>
                                     </div>
-                                    <time>{moment(item.date_added).tz('Asia/Tokyo').format('DD-MM-YYYY LTS')}</time>
+                                    <time>{moment(item.date_added).tz(this.state.timeZone).format('DD-MM-YYYY LTS')}</time>
                                 </div>
                             </div>
                         ))
                     }
                 </div>
                 <div className="control-arrange">
-                    <div className="btn-arrange" id="asc">Ascending Time</div>
-                    <div className="btn-arrange" id="desc">Descending Time</div>
+                    <div className="btn-arrange" data-sort-type="asc" data-column="date_added" onClick={this.sort.bind(this)}>Ascending Time</div>
+                    <div className="btn-arrange" data-sort-type="desc" data-column="date_added" onClick={this.sort.bind(this)}>Descending Time</div>
+                </div>
+                <div className="control-arrange">
+                    <div className="btn-arrange" data-sort-type="asc" data-column="name" onClick={this.sort.bind(this)}>Ascending Name</div>
+                    <div className="btn-arrange" data-sort-type="desc" data-column="name" onClick={this.sort.bind(this)}>Descending Name</div>
                 </div>
             </div>
         );
--- level2/src/reducers/skill.js
+++ level2/src/reducers/skill.js
@@ -2,37 +2,37 @@ let initialSkillsState = [
     {
         id: 1,
         avatar: '/images/1.jpg',
-        name: 'One',
+        name: '2017',
         tagline: 'If you have a bad day, catch a wave',
         date_added: '2017-09-01T02:31:12Z'
     },
     {
         id: 2,
         avatar: '/images/2.jpg',
-        name: 'Two',
+        name: '2015',
         tagline: 'I am Two f',
         date_added: '2015-01-02T08:00:10Z'
     },
     {
         id: 3,
         avatar: '/images/3.jpg',
-        name: 'Three',
+        name: '2011',
         tagline: 'I am Three',
         date_added: '2011-05-01T08:02:07Z'
     },
     {
         id: 4,
         avatar: '/images/4.jpg',
-        name: 'Four',
+        name: '2016',
         tagline: 'I am Four',
         date_added: '2016-12-14T08:12:05Z'
     },
     {
         id: 5,
         avatar: '/images/5.jpg',
-        name: 'Five',
+        name: '2018',
         tagline: 'I am Five',
-        date_added: '2017-05-05T08:08:10Z'
+        date_added: '2018-05-05T08:08:10Z'
     }
 ];
 
