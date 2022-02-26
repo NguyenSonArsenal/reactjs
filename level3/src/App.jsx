5cd3f01e2e66be91893eb120f3ff9c17556a5e5a
Nguyen Son Arsenal <vanson297.nguyen@gmail.com>
2022-02-26 10:09:12 +0700

    [l3] add show more/less text

--- level3/src/App.jsx
+++ level3/src/App.jsx
@@ -1,66 +1,67 @@
 import React from "react";
 
-
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            expanded: false
+            maxHeight: 40,
+            showMore: false,
+            showBtnControl: false,
+            content: '',
+            height: ''
         }
     }
 
+    componentDidUpdate() {
+        this.updateHeight();
+    }
+
+    updateHeight() {
+        let height = this.div ? this.div.scrollHeight : 0;
+        let status = this.state.height > this.state.maxHeight ? true : false;
+        if (this.state.height != height) {
+            this.setState({
+                height: height,
+                showBtnControl: status,
+                showMore: status,
+            })
+        }
+    }
+
+    handleChange = (e) => {
+        let status = this.state.height > this.state.maxHeight ? true : false;
+        this.setState({
+            content: e.target.value,
+            showBtnControl: status,
+            showMore: status
+        })
+    };
+
+    handleCollapsedContent = () => {
+        let isShowMore = this.state.showMore;
+        this.setState({
+            showMore: !isShowMore
+        })
+    };
+
     render(){
-        const { expanded } = this.state;
-        const toggledClass = expanded ? 'expanded' : 'collapsed';
+        let button = '';
+        if (this.state.showBtnControl) {
+            button = this.state.showMore == true ? 'More' : 'Less';
+        }
+        let className = this.state.showMore ? 'content collapsed' : 'content';
+
         return(
-            <div>
-                <div className={`content ${toggledClass}`}>
-                    Giờ thì tình đã phai
-                    Em đâu rồi người về với ai
-                    Còn lại mình lẻ loi
-                    Riêng mình tôi ôm nỗi đau này
-                    Còn lại gì nữa đâu
-                    Ừ thì còn lại gì nữa đâu
-                    Giờ thì đành mất em
-                    Riêng mình tôi đợi chờ
-                    Người ơi hãy tin anh này
-                    Và em đến đây đi
-                    Ngàn hoa lá đang thay màu
-                    Này em thích hoa gì?
-                    Nụ cười bé xinh xuân ngời
-                    Người làm xao xuyến tim anh
-                    Tình anh sẽ không phai mờ
-                    Vì anh đã trao dành
-                    Một ngày, một ngày hạ buồn kèm phượng hồng khi còn vương nắng
-                    Bầu trời một màu tình yêu, thu vừa sang lá xanh phai màu
-                    Đọng lại một chút nhớ thương đang rơi rơi trên khóe mi lệ buồn
-                    Chuyện tình ta bao lâu rồi đây mai cách xa
-                    Từng ngày, từng ngày còn gì, vừa rụng rơi khi mùa đông đến
-                    Là ngày mà người rời xa theo người ta bão giông nơi này
-                    Chẳng còn đâu những vấn vương, theo hương hoa bóng ai phai mờ
-                    Lời chia tay em chưa từng nói với anh
-                    Cớ sao người vội đi
-                    Người ơi hãy tin anh này
-                    Và em đến đây đi
-                    Ngàn hoa lá đang thay màu
-                    Này em thích hoa gì
-                    Nụ cười bé xinh xuân ngời
-                    Người làm xao xuyến tim anh
-                    Tình anh sẽ không phai mờ
-                    Vì anh đã trao dành
-                    Một ngày, một ngày hạ buồn kèm phượng hồng khi còn vương nắng
-                    Bầu trời một màu tình yêu, thu vừa sang lá xanh phai màu
-                    Đọng lại một chút nhớ thương đang rơi rơi trên khóe mi lệ buồn
-                    Chuyện tình ta bao lâu rồi đây mai cách xa
-                    Từng ngày, từng ngày còn gì, vừa rụng rơi khi mùa đông đến
-                    Là ngày mà người rời xa theo người ta bão giông nơi này
-                    Chẳng còn đâu những vấn vương theo hương hoa bóng ai phai mờ
-                    Lời chia tay em chưa từng nói với anh
-                    Cớ sao người vội đi
+            <div className="wrapper">
+                <div className="component">
+                    <div className={className} ref={ div => { this.div = div; } } >
+                        { this.state.content}
+                    </div>
+                    <div className="button"  style={{ color: "blue", cursor: "pointer" }} onClick={this.handleCollapsedContent}>
+                        {button}
+                    </div>
                 </div>
-                <button onClick={() => this.setState({ expanded: !expanded })}>
-                    {expanded ? 'View Less' : 'View More'}
-                </button>
+                <textarea name="" id="" cols="30" rows="10" onChange={this.handleChange}></textarea>
             </div>
         )
     }
